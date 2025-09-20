37bdec7cb55c20ea8ca19918c009e3cd2a852216
Charlie <schen622@student.ubc.ca>
2025-09-20 14:19:42 -0700

    made the colour and vec3 utility class.

--- main.cpp
+++ main.cpp
@@ -6,12 +6,13 @@ int main()
     int image_height = 256;
 
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
-    for (int j=0; j < image_height; j++)
+    for (int j = 0; j < image_height; j++)
     {
-        for (int i = 0; i<image_width; i++)
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        for (int i = 0; i < image_width; i++)
         {
-            auto r = double(i)/(image_width -1);
-            auto g = double(j)/(image_height -1);
+            auto r = double(i) / (image_width - 1);
+            auto g = double(j) / (image_height - 1);
             auto b = 0.0;
             int ir = int(255.999 * r);
             int ig = int(255.999 * g);
@@ -19,5 +20,5 @@ int main()
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
-}#!/bin/bash
-./cmake-build-debug/tiny_ray_tracer > image.ppm
+    std::clog << "\rDone.                 \n";
+}
