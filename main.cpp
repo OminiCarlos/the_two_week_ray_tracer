4cd3f77178515cf506dfecbdada3e15a9f6d24e6
Charlie <schen622@student.ubc.ca>
2025-09-20 18:31:41 -0700

    Ray, view port camera are implemented.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
+#include "vec3.h"
+#include "color.h"
+#include "ray.h"
+
+color ray_color(const ray& r){
+    return {0,0,0};
+}
 
 int main()
 {
-    int image_width = 256;
-    int image_height = 256;
+    // Image
+    auto aspect_ratio = 16.0 / 9.0;
+    int image_width = 400;
+    int image_height = static_cast<int>(image_width / aspect_ratio);
+    image_height = (image_height < 1)? 1:image_height;
+
+    // Camera
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * ((double)image_width/image_height);
+    auto camera_center = point3(0,0,0);
+
+    auto viewport_u = vec3(viewport_width, 0, 0);
+    auto viewport_v = vec3(0, -viewport_height, 0);
+
+    auto pixel_delat_u =  viewport_u / image_width;
+    auto pixel_delta_v =  viewport_v / image_height;
+
+    auto viewport_upper_left = camera_center - vec3(0,0,focal_length) - viewport_u/2 - viewport_v/2;
+    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delat_u + pixel_delta_v);
 
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
     for (int j = 0; j < image_height; j++)
@@ -11,13 +36,12 @@ int main()
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; i++)
         {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0.0;
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            auto pixel_center = pixel00_loc + (i*pixel_delat_u + j*pixel_delta_v);
+            auto ray_direction = pixel_center - camera_center;
+            ray r(camera_center, ray_direction);
+
+            color pxiel_color = ray_color(r);
+            write_color(std::cout, pxiel_color);
         }
     }
     std::clog << "\rDone.                 \n";
