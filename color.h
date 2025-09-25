f80f25dc427b587e83962c8978622e4f9f3dd1c7
Charlie <schen622@student.ubc.ca>
2025-09-24 23:26:32 -0700

    refactored the camera into its own class and added random resampling to achieve anti-aliasing.Now up to section 9.

--- color.h
+++ color.h
@@ -6,6 +6,7 @@
 #define TINY_RAY_TRACER_COLOR_H
 
 #include "vec3.h"
+#include "interval.h"
 
 using color = vec3;
 
@@ -15,9 +16,10 @@ inline void write_color(std::ostream& out, const color& pixel_color) {
     auto b = pixel_color.z();
 
     // Translate the [0,1] component values to the byte range [0,255].
-    int rbyte = int(255.999 * r);
-    int gbyte = int(255.999 * g);
-    int bbyte = int(255.999 * b);
+    static const interval intensity(0.000,0.999);
+    const int rbyte = static_cast<int>(256 * intensity.clamp(r));
+    const int gbyte = static_cast<int>(256 * intensity.clamp(g));
+    const int bbyte = static_cast<int>(256 * intensity.clamp(b));
 
     // Write out the pixel color components.
     out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
